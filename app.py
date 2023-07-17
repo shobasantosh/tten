6a8b4c6a69fd54a3c949af5f3d98a0120d2fc80c
Shoba Santosh <shoba@Shobas-MacBook-Pro.local>
2023-07-16 18:03:24 -0700

    app typo

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as plx
 
-data = pd.readcsv('vehicles_us.csv')
+data = pd.read_csv('vehicles_us.csv')
 
 st.header('Welcome to my sprint 4 project!!')
 
